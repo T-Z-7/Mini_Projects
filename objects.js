83f2f246f5b29e228dbbd8191e4140a32fd8b585
T-Z-7 <tmin75828@gmail.com>
2024-10-28 20:42:30 +0630

    add files

--- objects.js
+++ objects.js
@@ -1,10 +1,21 @@
+// objects = A collection related properties and/or method 
+//           Can represent real world objects(people,products,place)
+//           Object = {key:value,
+//                     function()}
+
+
+
 const person1 = {
     name : "Thaw Zin",
     age : 25,
     gender : "Male",
     sayHello : function(){
         console.log("Hello My name is Thaw Zin.")
+    },
+    eat : ()=>
+        console.log("I'm eating an egg...")
     }
-}
 
-console.log(person1)
+
+person1.sayHello();
+person1.eat();
---
+++ this.js
@@ -0,0 +1,11 @@
+
+
+const  person1 = {
+    name : "Thaw Zin" ,
+    work : "student",
+    sayHello : function(){
+        console,log(`Hello I'm ${this.name}.I am a ${this.work}`)
+    },
+}
+
+person1.sayHello();
